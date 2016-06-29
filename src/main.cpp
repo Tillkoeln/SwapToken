06bdffff6313291d5fda70e8323b9feb8a3c5031
Tillkoeln <Tillkoeln@coin.com>
2016-06-29 04:09:39 +0200

    8

--- src/main.cpp
+++ src/main.cpp
@@ -810,7 +810,7 @@ int CMerkleTx::GetBlocksToMaturity() const
 {
     if (!(IsCoinBase() || IsCoinStake()))
         return 0;
-    return max(0, (nCoinbaseMaturity+1440) - GetDepthInMainChain());
+    return max(0, (nCoinbaseMaturity+400) - GetDepthInMainChain());
 }
 
 
--- src/qt/transactionrecord.h
+++ src/qt/transactionrecord.h
@@ -72,7 +72,7 @@ public:
     };
 
     /** Number of confirmation recommended for accepting a transaction */
-    static const int RecommendedNumConfirmations = 1440;
+    static const int RecommendedNumConfirmations = 400;
 
     TransactionRecord():
             hash(), time(0), type(Other), address(""), debit(0), credit(0), idx(0)
